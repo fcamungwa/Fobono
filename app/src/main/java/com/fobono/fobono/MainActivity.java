4b43a6b4b85d4202cd076ca6ce2d4681d094e02f
El Reaper <fcamungwa@gmail.com>
2018-04-21 14:52:37 -0500

    Menu items added

--- app/src/main/java/com/fobono/fobono/MainActivity.java
+++ app/src/main/java/com/fobono/fobono/MainActivity.java
@@ -2,20 +2,59 @@ package com.fobono.fobono;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+    WebView browser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WebView browser = (WebView) findViewById(R.id.browser);
+        browser = findViewById(R.id.browser);
         browser.loadUrl("http://fobono.com");
         browser.getSettings().setJavaScriptEnabled(true);
+        browser.setWebChromeClient(new WebChromeClient());
         browser.setWebViewClient(new WebViewClient());
 
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.fobono_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.goback:
+                onBackPressed();
+                break;
+            case R.id.goforward:
+                if(browser.canGoForward())
+                    browser.goForward();
+                break;
+            case R.id.refresh:
+                browser.reload();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (browser.canGoBack())
+            browser.goBack();
+        else
+            finish();
     }
 }
